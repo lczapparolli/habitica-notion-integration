29ace24a68cf9ba13f439e38f612b95a76873148
Luis Carlos Zapparolli Neto <luis.zapparolli@externo.cresol.coop.br>
2022-08-30 15:22:14 -0300

    Defining macro process for syncing data

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Main function to get updates from Habitica and Notion and sync
+ */
+function syncHabiticaAndNotion() {
+    //Get Notion data
+    //Get Habitica data
+
+    //Generate differences
+    
+    //Update Notion
+    //Update Habitica
+}
